1e820c0d6cfdf0bc3d7b31511bef1b67e429ef0a
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-18 12:24:51 +0700

    01. Starter Project

---
+++ src/main/java/com/iffat/springchadmvc/SpringChadMvcApplication.java
@@ -0,0 +1,13 @@
+package com.iffat.springchadmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChadMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringChadMvcApplication.class, args);
+    }
+
+}
