feb94567f1949b17a47d682dbe65315b97244c30
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-18 15:19:34 +0700

    04. Hello World Form And Model

--- src/main/java/com/iffat/springchadmvc/controller/DemoController.java
+++ src/main/java/com/iffat/springchadmvc/controller/DemoController.java
@@ -3,6 +3,7 @@ package com.iffat.springchadmvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Date;
 
@@ -12,6 +13,16 @@ public class DemoController {
     @GetMapping("/hello")
     public String sayHello(Model model) {
         model.addAttribute("date", new Date());
+        return "helloworld";
+    }
+
+    @RequestMapping("/showForm")
+    public String showForm() {
+        return "helloworld-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm() {
         return "helloworld";
     }
 }
